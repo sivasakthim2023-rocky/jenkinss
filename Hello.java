6f571aa1600f86ed55a15887fa8508f446608854
sivasakthim2023-rocky <sivasakthi.m2023@vitstudent.ac.in>
2026-02-18 19:04:16 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Mini CI Project Running");
+        System.out.println("Mini CI Project Running")
     }
 }
